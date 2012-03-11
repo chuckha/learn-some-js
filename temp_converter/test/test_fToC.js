c09aea246699ededd2163e13e6e28c6e777dfc3b
chuck <ha.chuck@gmail.com>
2012-03-10 21:56:39 -0500

    removed old test file

--- temp_converter/test/test_fToC.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  module("Fahrenheit to Celsius");
-    
-  test("a basic test example", function() {
-    ok( true, "this test is fine" );
-    var value = "hello";
-    equal( value, "hello", "We expect value to be hello" );
-  });
-});
